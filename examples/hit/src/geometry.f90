8461ee6cbca3d2075cb920236020f6265cc5469d
jessecaps <jcaps@umich.edu>
2023-02-08 15:03:37 -0500

    remove p3dfft decomp
    Thought I removed this

--- examples/hit/src/geometry.f90
+++ examples/hit/src/geometry.f90
@@ -41,12 +41,11 @@ contains
       ! Create a config from that grid on our entire group
       create_cfg: block
         use parallel, only: group
-        use pgrid_class, only: p3dfft_decomp
          integer, dimension(3) :: partition
          ! Read in partition
          call param_read('Partition',partition,short='p')
          ! Create partitioned grid
-         cfg=config(grp=group,decomp=partition,grid=grid,strat=p3dfft_decomp)
+         cfg=config(grp=group,decomp=partition,grid=grid)
       end block create_cfg
       
       ! Create masks for this config
